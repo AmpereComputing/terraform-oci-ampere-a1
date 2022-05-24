af40e0474559268fdbc19d77023fe35f69345a82
Peter Pouliot <peter@pouliot.net>
2022-05-24 14:12:32 -0400

    changing output names

--- output.tf
+++ output.tf
@@ -1,17 +1,17 @@
 # Output the private and public IPs of the instance
 
-output "AmpereA1_PrivateIPs" {
+output "ampere_a1_private_ips" {
   value     = ["${oci_core_instance.ampere_a1.*.private_ip}"]
   sensitive = false
 }
 
-output "AmpereA1_PublicIPs" {
+output "ampere_a1_public_ips" {
   value     = ["${oci_core_instance.ampere_a1.*.public_ip}"]
   sensitive = false
 }
 
 # Output the boot volume IDs of the instance
-output "AmpereA1_BootVolumeIDs" {
+output "ampere_a1_boot_volume_ids" {
   value     = ["${oci_core_instance.ampere_a1.*.boot_volume_id}"]
   sensitive = false
 }
