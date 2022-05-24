e8ca195ee1d019180f74c25aa3f07db3af4a5fca
Peter Pouliot <peter@pouliot.net>
2022-05-24 15:26:15 -0400

    removing vars from default metadata

--- metadata.tf
+++ metadata.tf
@@ -10,11 +10,6 @@ locals {
 data "template_file" "cloud_config" {
 # template = file("${path.module}/templates/cloud-init.yaml.tpl")
   template = file("${local.cloud_init_template_path}")
-  vars = {
-    tf_ssh_privkey = tls_private_key.oci.private_key_pem
-    tf_ssh_pubkey = tls_private_key.oci.public_key_openssh
-    tf_random_id = random_uuid.random_id.result
-  }
 }
 
 output "cloud_init" {
